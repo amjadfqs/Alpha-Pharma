36a66046146cdd395b09bae26409545e827b1d06
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2021-12-11 23:44:16 +0300

    Done Adding Splash Screen

---
+++ Alpha Pharma/Forms/Loading.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Alpha_Pharma.Forms
+{
+    partial class Loading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progres_bar = new Guna.UI2.WinForms.Guna2CircleProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progres_bar
+            // 
+            this.progres_bar.BackColor = System.Drawing.Color.Transparent;
+            this.progres_bar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(167)))), ((int)(((byte)(214)))));
+            this.progres_bar.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progres_bar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(205)))), ((int)(((byte)(144)))));
+            this.progres_bar.Location = new System.Drawing.Point(112, 200);
+            this.progres_bar.Name = "progres_bar";
+            this.progres_bar.ProgressBrushMode = Guna.UI2.WinForms.Enums.BrushMode.Solid;
+            this.progres_bar.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(205)))), ((int)(((byte)(144)))));
+            this.progres_bar.ProgressEndCap = System.Drawing.Drawing2D.LineCap.Round;
+            this.progres_bar.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.progres_bar.ShadowDecoration.Parent = this.progres_bar;
+            this.progres_bar.ShowPercentage = true;
+            this.progres_bar.Size = new System.Drawing.Size(297, 258);
+            this.progres_bar.TabIndex = 0;
+            this.progres_bar.UseTransparentBackground = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Loading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Alpha_Pharma.Properties.Resources.Loading;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(521, 670);
+            this.Controls.Add(this.progres_bar);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Loading";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loading";
+            this.Load += new System.EventHandler(this.Loading_Load);
+            this.Shown += new System.EventHandler(this.Loading_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CircleProgressBar progres_bar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Alpha Pharma/Forms/Loading.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alpha_Pharma.Forms
+{
+    public partial class Loading : Form
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+
+        private void Loading_Shown(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Loading_Load(object sender, EventArgs e)
+        {
+            progres_bar.Value = 0;
+            progres_bar.Minimum = 0;
+            progres_bar.Maximum = 100;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            progres_bar.Increment(5);
+            if (progres_bar.Value == 100)
+            {
+                timer1.Stop();
+                this.Hide();
+            }
+
+        }
+    }
+}
