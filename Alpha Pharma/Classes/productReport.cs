ce0d70b1fd4edd04b3c34818320f1fdecf80c2ed
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2021-12-21 04:12:41 +0300

    donr

--- Alpha Pharma/Classes/productReport.cs
+++ Alpha Pharma/Classes/productReport.cs
@@ -12,14 +12,14 @@ namespace Alpha_Pharma.Classes
                                                      "Join Products as P on S.pro_id = P.pro_id " +
                                                      "Join Suppliers as Sp on S.sup_id = Sp.sup_id " +
                                                      "join Forms as F on P.for_id = F.for_id " +
-                                                     "join Doses as D on P.dos_id = D.dos_id where pro_exp <= Cast(getdate() as date)";
+                                                     "join Doses as D on P.dos_id = D.dos_id where pro_exp >= Cast(getdate() as date)";
 
         private const string SelectValidProducts = "Select sto_id as StoreID,(pro_name + ' , ' + for_name + ' , ' + dos_qty) as DrugInfo, sup_name as SupplierName, pro_exp as Product_Expired , " +
                                                    "pro_price as Product_Price, pro_qty as Product_Quantity ,(pro_price*pro_qty) as Total_Price, pro_code as Product_Code from Store As S " +
                                                    "Join Products as P on S.pro_id = P.pro_id " +
                                                    "Join Suppliers as Sp on S.sup_id = Sp.sup_id " +
                                                    "join Forms as F on P.for_id = F.for_id " +
-                                                   "join Doses as D on P.dos_id = D.dos_id where pro_exp >= Cast(getdate() as date)";
+                                                   "join Doses as D on P.dos_id = D.dos_id where pro_exp <= Cast(getdate() as date)";
 
         private const string SelectAllProducts = "Select sto_id as StoreID,(pro_name + ' , ' + for_name + ' , ' + dos_qty) as DrugInfo, sup_name as SupplierName, pro_exp as Product_Expired , " +
                                                  "pro_price as Product_Price, pro_qty as Product_Quantity ,(pro_price*pro_qty) as Total_Price, pro_code as Product_Code from Store As S " +
